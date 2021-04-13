a63f0fa15de3d70f3b9c3a400164b4d13d832099
jerrymk <jerrymonkeyinc@gmail.com>
2021-04-13 20:35:30 +0800

    Removed testing code

---
+++ Graphene/Source/Application.cpp
@@ -0,0 +1,59 @@
+﻿#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+#include "GUE/GUE.h"
+#include "ExceptionHandler.hpp"
+
+
+int main() {
+	gue::Window window;
+	if (!window.createWindow(Vec2u(1024, 768), u8"Graphene α")) {
+		ERR("failed to create window");
+	}
+
+	gue::VertexArray vertexArray(window);
+
+	gue::Shader shader;
+	shader.setShaderSource(shader.defaultVertexShader, shader.defaultFragmentShader);
+	shader.loadShader();
+
+
+	glfwSetInputMode(window.getGLFWWindow(), GLFW_STICKY_KEYS, GL_TRUE);
+
+	ExceptionHandler eh;
+	eh.flushExceptionsToIOStream();
+
+	gue::Timer animTimer;
+
+	gue::FramerateCounter framerateCounter;
+
+	do {
+		GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+		vertexArray.clear();
+
+		
+
+		vertexArray.draw(shader);
+
+		//framerate counter
+		framerateCounter.frameCount();
+		DBG(std::to_string(framerateCounter.getFramerate()) + "fps\tTriangles: " + std::to_string(vertexArray.getVertices()->size() / 3));
+
+		//DBG(std::to_string(vertexArray.getVertices()->size()));
+
+		// Swap buffers
+		GLCall(glfwSwapBuffers(window.getGLFWWindow()));
+		GLCall(glfwPollEvents());
+
+		ExceptionHandler eh;
+		eh.flushExceptionsToIOStream();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while (glfwGetKey(window.getGLFWWindow(), GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window.getGLFWWindow()) == 0);
+
+}
