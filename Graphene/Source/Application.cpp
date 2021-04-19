30e68be482761e1997c59759419a0f144d92c718
jerrymk <jerrymonkeyinc@gmail.com>
2021-04-19 12:41:20 +0800

    Removed callback hpp

--- Graphene/Source/Application.cpp
+++ Graphene/Source/Application.cpp
@@ -10,8 +10,24 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "ExceptionHandler.hpp"
-#include "Callbacks.hpp"
 
+
+static void glfw_error_callback(int error, const char* description) {
+	ERR("GLFW Error: " + std::to_string(error) + std::string(description));
+}
+
+static void useStyle() {
+	ImGuiStyle* style = &ImGui::GetStyle();
+
+	style->FrameRounding = 6.0f;
+	style->GrabRounding = 4.0f;
+	style->PopupRounding = 4.0f;
+	style->ChildRounding = 4.0f;
+	style->TabRounding = 6.0f;
+	style->WindowRounding = 8.0f;
+
+	style->Colors[ImGuiCol_WindowBg].w = 1.0f;
+}
 
 int main() {
 
