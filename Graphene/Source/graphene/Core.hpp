7ca79af35b5dc7ce83e02e7282149df538673959
jerrymk <jerrymonkeyinc@gmail.com>
2021-05-13 22:35:22 +0800

    Fixed bug when vertex connected to itself, added comments

--- Graphene/Source/graphene/Core.hpp
+++ Graphene/Source/graphene/Core.hpp
@@ -18,18 +18,20 @@ namespace Graphene {
 
 	public:
 
-		Core(Graph& graph) {
+		// binds a graph to a core
+		explicit Core(Graph &graph) {
 			m_graph = &graph;
 		}
 
-		float m_c1 = 2;
-		float m_c2 = 1;
-		float m_c3 = 1;
-		float m_c4 = 0.1;
+		// force calculation constants
+		float c1 = 2;
+		float c2 = 1;
+		float c3 = 1;
+		float c4 = 0.1;
 
+		// graph updated via visual tool
 		bool pendingInputUpdate = false;
 
-		Vertex* grabbingVertex = nullptr;
 
 		Graph* boundGraph() {
 			return m_graph;
@@ -48,9 +50,9 @@ namespace Graphene {
 
 		Vec2f attractForce(Vertex* u, Vertex* v) {
 			float dis = distance(u, v);
-			if (dis == 0.0)
+			if (dis == 0.0f)
 				return Vec2f(0, 0);
-			float coeff = m_c1 * log(dis / m_c2);
+			float coeff = c1 * log(dis / c2);
 			return (v->getCoord() - u->getCoord()).normalize() * coeff;
 		}
 
@@ -58,17 +60,17 @@ namespace Graphene {
 			float dis = distance(u, v);
 			if (dis == 0.0)
 				return Vec2f(0, 0);
-			float coeff = -m_c3 / dis / dis;
+			float coeff = -c3 / dis / dis;
 			return (v->getCoord() - u->getCoord()).normalize() * coeff;
 		}
 
-        Vec2f originAttractForce(Vertex* v){
-		    float dis = v->getCoord().length();
-		    if(dis == 0.0){
-		        return Vec2f(0, 0);
-		    }
-            float coeff = std::min(1.0f, dis);
-		    return (Vec2f(0, 0) - v->getCoord()).normalize() * coeff;
+		Vec2f originAttractForce(Vertex* v) {
+			float dis = v->getCoord().length();
+			if (dis == 0.0) {
+				return Vec2f(0, 0);
+			}
+			float coeff = std::min(1.0f, dis);
+			return (Vec2f(0, 0) - v->getCoord()).normalize() * coeff;
 		}
 
 
@@ -79,26 +81,34 @@ namespace Graphene {
 				while (uIt.next()) {
 					VertexIter vIt(boundGraph());
 					while (vIt.next()) {
-                        uIt.v->move(repelForce(uIt.v, vIt.v));
-                        vIt.v->move(repelForce(vIt.v, uIt.v));
-                    }
+						uIt.v->move(repelForce(uIt.v, vIt.v));
+						vIt.v->move(repelForce(vIt.v, uIt.v));
+					}
 				}
 			}
+
 			{
 				EdgeIter it(boundGraph());
 				while (it.next()) {
-					float dis = distance(it.u, it.v);
 					it.u->move(attractForce(it.u, it.v));
-                    it.v->move(attractForce(it.v, it.u));
+					it.v->move(attractForce(it.v, it.u));
 				}
 			}
+
 			{
 				VertexIter it(boundGraph());
 				while (it.next()) {
-				    it.v->move(originAttractForce(it.v));
-                    if (it.v != grabbingVertex)
-                        it.v->flushMove(m_c4);
-                }
+					// TODO: Janky solution to connected components uncontrolled
+					it.v->move(originAttractForce(it.v));
+				}
+			}
+
+			{
+				VertexIter it(boundGraph());
+				while (it.next()) {
+					if (!it.v->pauseMove)
+						it.v->flushMove(c4);
+				}
 			}
 
 		}
